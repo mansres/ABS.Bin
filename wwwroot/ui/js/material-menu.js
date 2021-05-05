96b6dd3931d967657a2ed7457aa78cbfd7af03d6
Daniel Lozano <d.lozanonavas@outlook.com>
2021-05-05 07:32:26 -0500

    05/05/2021 06:54:29

--- wwwroot/ui/js/material-menu.js
+++ wwwroot/ui/js/material-menu.js
@@ -1,12 +1,17 @@
 document.addEventListener("touchstart", function () { }, false);
 $(function () {
 	$('.material-navtoggle').click(function () {
-		$('.material-menucontainer').toggleClass('wsoffcanvasopener');
+		$('.material-menucontainer').toggleClass('material-offcanvasopener');
+	});
+
+	$('#material-navtoggle').click(function () {
+		$('.material-menucontainer').toggleClass('material-offcanvasopener');
 	});
 
 	$('.overlapblackbg').click(function () {
-		$('.material-menucontainer').removeClass('wsoffcanvasopener');
+		$('.material-menucontainer').removeClass('material-offcanvasopener');
 	});
+
 
 
 });
