172a26bba247338d0897734ac9f823b3dd2d82a5
Daniel Lozano <d.lozanonavas@outlook.com>
2021-07-01 15:53:26 -0500

    Nightly build: 07/01/2021 15:51:44

---
+++ wwwroot/ui/lib/menu/material-menu.js
@@ -0,0 +1,15 @@
+document.addEventListener("touchstart", function () { }, false);
+$(function () {
+	$('.material-navtoggle').click(function () {
+		$('.material-menucontainer').toggleClass('material-offcanvasopener');
+	});
+
+	$('#material-navtoggle').click(function () {
+		$('.material-menucontainer').toggleClass('material-offcanvasopener');
+	});
+
+	$('.overlapblackbg').click(function () {
+		$('.material-menucontainer').removeClass('material-offcanvasopener');
+	});
+
+});
