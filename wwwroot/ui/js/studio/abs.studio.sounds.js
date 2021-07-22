80e6b193d85c2e1b7947a3fef0667fd73d9d1d07
Daniel Lozano <d.lozanonavas@outlook.com>
2021-07-22 11:07:38 -0500

    Nightly build: 07/22/2021 11:05:59

---
+++ wwwroot/ui/js/studio/abs.studio.sounds.js
@@ -0,0 +1,21 @@
+﻿window.SoundsManager =
+{
+    beep: function () {
+        window.SoundsManager.playSound("/ui/sounds/beep.mp3");
+    },
+    trash: function () {
+        window.SoundsManager.playSound("/ui/sounds/trash.mp3");
+    },
+    toast: function () {
+        window.SoundsManager.playSound("/ui/sounds/toast.mp3");
+    },
+    error: function () {
+        window.SoundsManager.playSound("/ui/sounds/error.mp3");
+    },
+    playSound: function (src) {
+        const sound = new Audio();
+        sound.src = src;
+        sound.play();
+        sound.onended = () => delete (sound);
+    },
+}
